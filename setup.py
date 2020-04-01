4ff0740cbbd0fd4767ef83a930a425e198e1abe4
HamzaZo <hamza.lil01@gmail.com>
2020-04-01 17:53:06 +0200

    rename executable

--- setup.py
+++ setup.py
@@ -5,6 +5,6 @@ setup(
     name = "unit",
     version = "0.1.0",
     description = "Helm unit plugin",
-    executables = [Executable("unit.py")]
+    executables = [Executable("helm-unit.py")]
     
 )
